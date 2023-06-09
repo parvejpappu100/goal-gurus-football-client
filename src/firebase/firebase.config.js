e886cee6b4104ff357bd0eac44e5b2753007f64e
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-06-09 22:49:28 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,14 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWqdHifNczDzvOSfTlODPatuLMMPlnI2g",
-  authDomain: "goal-gurus-football.firebaseapp.com",
-  projectId: "goal-gurus-football",
-  storageBucket: "goal-gurus-football.appspot.com",
-  messagingSenderId: "851944442487",
-  appId: "1:851944442487:web:00ec2645ff5a470f119472"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app;
+export default app
