357feabcb37ba9249cefd87c0dcc4a182c1006fa
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-06-11 01:58:32 +0600

    sort popular classes by enrolled student

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -15,7 +15,7 @@ const PopularClasses = () => {
             <h3 className='text-center text-4xl  mb-10'>Popular Classes</h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    popularClasses.slice(0, 6).map(popular => <div key={popular._id}>
+                    popularClasses.sort((a , b) => b.enrolled_students - a.enrolled_students).slice(0, 6).map(popular => <div key={popular._id}>
                         <div className='relative'>
                             <img className='h-[400px] w-full' src={popular.image} alt="" />
                             <h3 className='text-white inset-0 absolute pl-5 pt-10 w-full bg-black bg-opacity-20 h-full text-2xl'>{popular.name}</h3>
