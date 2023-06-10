a636439e3018c79705f9f7a67250510639599de2
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-06-11 00:33:44 +0600

    added popular coaches swiper on home page

--- src/pages/Home/PopularCoach/PopularCoach.jsx
+++ src/pages/Home/PopularCoach/PopularCoach.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "./PopularCoach.css"
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
 
 const PopularCoach = () => {
+
+    const [coaches, setCoaches] = useState([]);
+
+    useEffect(() => {
+        fetch("coaches.json")
+            .then(res => res.json())
+            .then(data => setCoaches(data))
+    }, [])
+    console.log(coaches)
     return (
         <div className=' bg-[#F5E1DA] pb-20'>
             <div className='lg:container mx-auto'>
                 <h3 className='text-4xl text-center'>Our Popular Coach</h3>
             </div>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={20}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper my-20 max-w-6xl mx-auto"
+            >
+                {
+                    coaches.slice(0 , 6).map(coach => <SwiperSlide key={coach._id}>
+                        <img src={coach.image} alt="" />
+                        <h3 className='text-[12px] md:text-xl font-semibold mt-5'>{coach.name}</h3>
+                    </SwiperSlide>)
+                }
+            </Swiper>
         </div>
     );
 };
