924ef03d44ee0247eb95f120660961980960a842
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-06-11 17:42:28 +0600

    slice classes on classes page

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -12,6 +12,11 @@ const Classes = () => {
             .then(data => setClasses(data))
     }, [])
 
+    const [showAll, setShowAll] = useState(false);
+    const handleShowAll = (event) => {
+        setShowAll(true);
+    };
+
     return (
         <div>
             <Banner
@@ -23,10 +28,17 @@ const Classes = () => {
                 <h3 className='text-4xl px-5 lg:px-0'>Our All Classes</h3>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-20'>
                     {
-                        allClasses.map(classes => <ShowAllClasses
-                        key={classes._id}
-                        classes={classes}
+                        allClasses.slice(0, showAll ? allClasses.length : 8).map(classes => <ShowAllClasses
+                            key={classes._id}
+                            classes={classes}
                         ></ShowAllClasses>)
+                    }
+                </div>
+                <div className='text-center my-8'>
+                    {
+                        !showAll && (
+                            <button onClick={handleShowAll} className=' hover:bg-[#173931] hover:text-white duration-500 border border-black py-2 px-7 uppercase rounded-3xl'>View All</button>
+                        )
                     }
                 </div>
             </div>
