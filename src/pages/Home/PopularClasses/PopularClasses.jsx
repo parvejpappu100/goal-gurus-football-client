e1e7433ddb6bd78027288f32087230e21caa43bb
parvejpappu-100 <parvejhasanpappu85@gmail.com>
2023-06-11 17:49:15 +0600

    added enrolled student number on popular classes

--- src/pages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/Home/PopularClasses/PopularClasses.jsx
@@ -15,10 +15,13 @@ const PopularClasses = () => {
             <h3 className='text-center text-4xl  mb-10'>Popular Classes</h3>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 {
-                    popularClasses.sort((a , b) => b.enrolled_students - a.enrolled_students).slice(0, 6).map(popular => <div key={popular._id}>
+                    popularClasses.sort((a, b) => b.enrolled_students - a.enrolled_students).slice(0, 6).map(popular => <div key={popular._id}>
                         <div className='relative'>
                             <img className='h-[400px] w-full' src={popular.image} alt="" />
-                            <h3 className='text-white inset-0 absolute pl-5 pt-10 w-full bg-black bg-opacity-20 h-full text-2xl'>{popular.name}</h3>
+                            <div className='text-white inset-0 absolute pl-5 pt-10 w-full bg-black bg-opacity-20 h-full text-2xl'>
+                                <h3>{popular.name}</h3>
+                                <span className='text-base'>Enrolled Student : {popular.enrolled_students}</span>
+                            </div>
                         </div>
                     </div>)
                 }
